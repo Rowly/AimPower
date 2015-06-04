1377ea8fb0d7e8d1b3c5f5dc7c19bd8c1807a2c4
Rowly <mail.rowlands@gmail.com>
2015-06-04 13:16:02 +0000

    Final 3 aim control ruby files

---
+++ src/root/nested/aim_restart_1.rb
@@ -0,0 +1,32 @@
+require 'selenium-webdriver'
+require 'headless'
+
+headless = Headless.new
+headless.start
+
+@driver = Selenium::WebDriver.for :firefox
+@wait = Selenium::WebDriver::Wait.new(:timeout => 60)
+
+@driver.navigate.to "http://10.10.l0.10/"
+
+def wait_for_element(locator)
+  element = @wait.until { @driver.find_element(locator) }
+  @wait.until { element.displayed? }
+  return element
+end
+
+element = wait_for_element(:css => "#username")
+element.send_keys "admin"
+
+element = wait_for_element(:css => "#password")
+element.send_keys "password"
+
+element = wait_for_element(:css => "#login")
+element.click
+
+wait_for_element(:link_text => "DASHBOARD")
+
+@driver.navigate.to "http://10.10.10.10/admin/process_power.php?mode=restar"
+
+
+@driver.quit
