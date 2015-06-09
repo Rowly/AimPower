25c763854f86de26a6e617e205d923037878cf15
Rowly <mail.rowlands@gmail.com>
2015-06-09 10:03:09 +0000

    Change over to python webdriver for improved logging of errors

--- src/root/nested/power_switch.py
+++ src/root/nested/power_switch.py
@@ -3,6 +3,11 @@ import time
 import requests
 from requests.auth import HTTPDigestAuth
 import os
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 LOGIN_FLAG = False
 SHUTDOWN_FLAG = False
@@ -38,45 +43,109 @@ def send_power_restart():
     r = requests.get("http://10.10.10.254/hidden.htm?M0:O1=Restart", auth=HTTPDigestAuth("api", "api"))
     assert(r.status_code == 200)
 
+def start_driver():
+    driver = webdriver.Firefox()
+    wait = WebDriverWait(driver, 30)
+    return driver, wait
+
 def aim_login():
     global LOGIN_FLAG
     LOGIN_FLAG = False
     logging.info("ADDER: Login Aim")
-    ret = os.system("ruby aim_login.rb")
-    if ret == 0:
-        LOGIN_FLAG = True
-    else:
-        logging.info("ADDER: Problem with Aim Login")
+    try:
+        driver, wait = start_driver()
+        driver.navigate.to("http://10.10.10.10/")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#username"))).send_keys("admin")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#password"))).send_keys("password")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#login"))).click()
+        LOGIN_FLAG = wait.until(EC.presense_of_element_located((By.LINK_TEXT, "DASHBOARD"))).is_displayed()
+    except TimeoutException as e:
+        logging.info("ADDER: Problem with Aim Login - Stacktrace: %s" %e)
+    finally:
+        driver.quit()
+    # ret = os.system("ruby aim_login.rb")
+    # if ret == 0:
+    #     LOGIN_FLAG = True
+    # else:
+    #     logging.info("ADDER: Problem with Aim Login")
     
 def aim_shutdown():
     global SHUTDOWN_FLAG
     SHUTDOWN_FLAG = False
     logging.info("ADDER: Login and Shutdown Aim")
-    ret = os.system("ruby aim_shutdown.rb")
-    if ret == 0:
-        SHUTDOWN_FLAG = True
-    else:
-        logging.info("ADDER: Problem with Aim Shutdown")  
+    try:
+        driver, wait = start_driver()
+        driver.navigate.to("http://10.10.10.10/")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#username"))).send_keys("admin")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#password"))).send_keys("password")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#login"))).click()
+        wait.until(EC.presense_of_element_located((By.LINK_TEXT, "DASHBOARD"))).is_displayed()
+        driver.navigate.to("http://10.10.10.10/admin/process_power.php?mode=shutdown")
+        time.sleep(5)
+        driver.refresh()
+        found = driver.find_elements((By.LINK_TEXT, "DASHBOARD"))
+        if len(found) == 0:
+            SHUTDOWN_FLAG = True
+    except TimeoutException as e:
+        logging.info("ADDER: Problem with Aim Shutdown - Stacktrace: %s" %e)
+    finally:
+        driver.quit()
+    # ret = os.system("ruby aim_shutdown.rb")
+    # if ret == 0:
+    #     SHUTDOWN_FLAG = True
+    # else:
+    #     logging.info("ADDER: Problem with Aim Shutdown")  
 
 def aim_restart():
     global RESTART_FLAG
     RESTART_FLAG = False
     logging.info("ADDER: Login and Restart Aim")
-    ret = os.system("ruby aim_restart.rb")
-    if ret == 0:
-        RESTART_FLAG = True
-    else:
-        logging.info("ADDER: Problem with Aim Restart")
+    try:
+        driver, wait = start_driver()
+        driver.navigate.to("http://10.10.10.10/")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#username"))).send_keys("admin")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#password"))).send_keys("password")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#login"))).click()
+        wait.until(EC.presense_of_element_located((By.LINK_TEXT, "DASHBOARD"))).is_displayed()
+        driver.navigate.to("http://10.10.10.10/admin/process_power.php?mode=restart")
+        time.sleep(20)
+        driver.refresh()
+        RESTART_FLAG = wait.until(EC.presense_of_element_located((By.LINK_TEXT, "DASHBOARD"))).is_displayed()
+    except TimeoutException as e:
+        logging.info("ADDER: Problem with Aim Restart - Stacktrace: %s" %e)
+    finally:
+        driver.quit()
+    # ret = os.system("ruby aim_restart.rb")
+    # if ret == 0:
+    #     RESTART_FLAG = True
+    # else:
+    #     logging.info("ADDER: Problem with Aim Restart")
     
 def aim_reset():
     global RESET_FLAG
     RESET_FLAG = False
     logging.info("ADDER: Login and Reset Aim")
-    ret = os.system("ruby aim_reset.rb")
-    if ret == 0:
-        RESET_FLAG = True
-    else:
-        logging.info("ADDER: Problem with Aim Reset")
+    try:
+        driver, wait = start_driver()
+        driver.navigate.to("http://10.10.10.10/")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#username"))).send_keys("admin")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#password"))).send_keys("password")
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#login"))).click()
+        wait.until(EC.presense_of_element_located((By.LINK_TEXT, "DASHBOARD"))).is_displayed()
+        wait.until(EC.presense_of_element_located((By.LINK_TEXT, "UPDATES"))).click()
+        wait.until(EC.presense_of_element_located((By.LINK_TEXT, "Reset AIM Configuration"))).click()
+        wait.until(EC.presense_of_element_located((By.CSS_SELECTOR, "#confirm_reset_link"))).click()
+        time.sleep(120)        
+        RESET_FLAG = wait.until(EC.presense_of_element_located((By.LINK_TEXT, "DASHBOARD"))).is_displayed()
+    except TimeoutException as e:
+        logging.info("ADDER: Problem with Aim Reset - Stacktrace: %s" %e)
+    finally:
+        driver.quit()
+    # ret = os.system("ruby aim_reset.rb")
+    # if ret == 0:
+    #     RESET_FLAG = True
+    # else:
+    #     logging.info("ADDER: Problem with Aim Reset")
 
 if __name__ == "__main__":
     logging_start()
